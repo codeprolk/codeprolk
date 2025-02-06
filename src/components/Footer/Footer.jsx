7f932d2915646996659427b47626085f77d8f088
Dinesh <dineshpiyasamara@gmail.com>
2025-02-06 23:27:55 +0530

    init

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import footerLogo from "../../assets/logo.png";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+  FaVoicemail,
+} from "react-icons/fa";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="text-black">
+      <div className="container">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-20 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="" className="max-w-[50px]" />
+              CODEPRO LK
+            </h1>
+            <p>
+              Helping talented individuals to reach the International Market.
+            </p>
+          </div>
+
+          {/* Footer Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-black"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-black"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* social links */}
+
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Kolonnawa, Western Province, Sri Lanka</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaVoicemail />
+                  <p>info@codeprolk.com</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt />
+                  <p>+9477 087 4042</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
