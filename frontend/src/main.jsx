86051431ed75e4d39a6e1d1408023d148aea4b04
neelia26 <saaramakuloluwa@gmail.com>
2026-09-14 12:05:24 +0530

    Seperated Styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { loadSlim } from '@tsparticles/slim';
 
 import App from './App.jsx';
 import './styles.css';
-import './blog.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
